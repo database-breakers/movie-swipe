e6945bcacba30485f7705737d48a2394a432f6a2
png <jasona99@outlook.com>
2020-10-24 18:26:42 -0400

    Simple express setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+const port = process.env.PORT;
+
+app.get('/', (req,res) => {
+	res.send("Hello world!");
+})
+
+app.listen(port, ()=>{
+	console.log(`Listening on ${port}`);
+})
