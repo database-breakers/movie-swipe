dfe84eed83d6c515eb3ae2ec90e7851463aab648
png <jasona99@outlook.com>
2020-10-24 18:34:48 -0400

    DB connection

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 require('dotenv').config()
 
 const port = process.env.PORT;
+
+var con = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD
+});
+
+con.connect(function(err){
+	if(err) throw err;
+	console.log("Connected!");
+});
 
 app.get('/', (req,res) => {
 	res.send("Hello world!");
