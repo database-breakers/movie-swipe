1c39583eae3907ac364318234eea1abae4a91d5d
png <jasona99@outlook.com>
2020-10-24 21:02:38 -0400

    Some DB connection work and a Procfile

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const port = process.env.PORT;
 var con = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD
+	password: process.env.DB_PASSWORD,
+	database: "movieswipe"
 });
 
 con.connect(function(err){
