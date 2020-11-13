cc9546ef1308ccba86cd2c999ef02a1d7cf479b3
png <jasona99@outlook.com>
2020-11-13 06:28:54 -0500

    Demo of loading API on component mount

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,6 +2,16 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class HomeScreen extends Component {
+    componentDidMount() {
+        const apiUrl = '/api/movies/v1/tt0120737?full=1';
+        fetch(apiUrl)
+            .then((response) => {
+                console.log(response);
+                response.json()
+            } )
+            .then((data) => console.log('This is your data', data));
+    }
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
