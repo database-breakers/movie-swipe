c5726305a178fe9f7a40c8c0813288052376cc37
Rickey Guo <32823151+rickeyguo@users.noreply.github.com>
2020-11-15 14:42:51 -0500

    login screen banner added but width is too wide

--- client/components/HomeHeader.js
+++ client/components/HomeHeader.js
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import { Surface, Text } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
-
+import swipelogo from '../assets/swipelogo.svg'
 const MyComponent = () => (
   <Surface style={styles.surface}>
-     <Text>Header</Text>
+    
+     <Text style={{ 
+       fontWeight: 'bold', 
+       color: '#FFFFFF',
+       fontSize: 40
+       }}>
+         <img src={swipelogo} alt = "movie swipe logo" style = {{height: 50}} />
+  Movie Swipe {"\n"}
+  
+  <Text style={{ 
+    fontWeight: 'bold', 
+    color: '#FFBB12',
+    fontSize: 20
+    }}>Swipe to agree on what to watch</Text>
+</Text>
   </Surface>
 );
 
@@ -12,12 +26,19 @@ export default MyComponent;
 
 const styles = StyleSheet.create({
   surface: {
-    padding: 8,
-    height: 80,
-    width: 80,
+    padding: 20,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     elevation: 4,
-    backgroundColor: '#0E4DA4'
+    color: '#FFFFFF',
+    backgroundColor: '#0E4DA4',
+    
+    baseText: {
+      fontWeight: 'bold'
+    },
+    innerText: {
+      color: 'red'
+    }
   },
 });
